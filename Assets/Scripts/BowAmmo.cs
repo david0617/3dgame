e69bc8b3ba515fd08452a879c079b7ffc137865c
俞天诚 <davidyu18800@gmail.com>
2018-11-23 11:03:09 -0800

    Fixs

---
+++ Assets/Scripts/BowAmmo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowAmmo : MonoBehaviour {
+	public int FlyTimer,StopTimer;
+	public bool usegravity;
+	public int Damage;
+	private GameObject rb;
+	// Use this for initialization
+	void Start () {
+		Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+		Destroy(gameObject, StopTimer);
+		if (usegravity){
+			StartCoroutine(AmmoFlyTime());
+		}
+	}
+	
+	 public IEnumerator AmmoFlyTime(){
+		yield return new WaitForSeconds(FlyTimer);
+	}
+	
+
+	public void OnTriggerEnter(Collider go){
+		PlayerHealth ph = go.gameObject.GetComponent<PlayerHealth>();
+		if (ph != null){
+			ph.DealDamage(Damage);
+		} 
+		Destroy(gameObject);
+	}
+}
