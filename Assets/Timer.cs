702773f39bdc3d7128e99bb7946d6bc108042f55
俞天诚 <davidyu18800@gmail.com>
2018-12-04 12:26:10 -0800

    TImer

---
+++ Assets/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	public int timeS,timeM;
+	public bool endLess;
+	public Text timerDisplay;
+	// Use this for initialization
+	void Start () {
+		if(endLess){
+			timeS = 0;
+			StartCoroutine(Times());
+		}else{
+			StartCoroutine(Timers());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeM.ToString();
+		timeS.ToString();
+		timerDisplay.text = timeM + ":" + timeS;
+	}
+	private IEnumerator Timers(){
+		yield return new WaitForSeconds(1);
+		timeS -- ;
+		if(timeS == 0){
+			timeM--;
+			timeS = 59;
+		}
+		StartCoroutine(Timers());
+	}
+
+	private IEnumerator Times(){
+		yield return new WaitForSeconds(1);
+		timeS ++;
+		if(timeS == 60){
+			timeM ++;
+			timeS = 0;
+		}
+		StartCoroutine(Times());
+	}
+}
