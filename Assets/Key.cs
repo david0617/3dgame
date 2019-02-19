6bfbd0b40e681275b61fc4a834a99b3125233baf
俞天诚 <davidyu18800@gmail.com>
2019-02-19 11:41:10 -0800

    Key

---
+++ Assets/Key.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public GameObject key;
+    private int prevWepIndex = 0;
+	private int curWepIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     public void OnTriggerEnter(Collider go)
+    {
+        PlayerHealth ph = go.gameObject.GetComponent<PlayerHealth>();
+        if (ph != null && key)
+        {
+			key.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
 	public int timeS,timeM;
-	public bool endLess;
+	public bool endLess,key;
 	public Text timerDisplay;
 	// Use this for initialization
 	void Start () {
@@ -13,8 +13,6 @@ public class Timer : MonoBehaviour {
 			timeS = 0;
 			timeM = 0;
 			StartCoroutine(Times());
-		}else{
-			StartCoroutine(Timers());
 		}
 	}
 	
@@ -27,12 +25,22 @@ public class Timer : MonoBehaviour {
 	private IEnumerator Timers(){
 		yield return new WaitForSeconds(1);
 		timeS -- ;
+		print(timeS);
 		if(timeS == 0 || timeS < 0){
 			timeM--;
 			timeS = 59;
 		}
 		StartCoroutine(Timers());
 	}
+
+	 public void OnTriggerEnter(Collider go)
+    {
+        PlayerHealth ph = go.gameObject.GetComponent<PlayerHealth>();
+        if (ph != null && key)
+        {
+			StartCoroutine(Timers());
+        }
+    }
 
 	private IEnumerator Times(){
 		yield return new WaitForSeconds(1);
