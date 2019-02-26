6a86f1d199951fbe691f62f005d7add1b9f85312
俞天诚 <davidyu18800@gmail.com>
2019-02-26 12:14:55 -0800

    Key Spawners

---
+++ Assets/Scripts/KeySpawners.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeySpawners : MonoBehaviour
+{
+    public GameObject[] keys;
+    public GameObject key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random key = new Random();
+//        int randomNumber = key.next (0,8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
