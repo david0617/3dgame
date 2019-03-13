1f0fd4b9c15859f5a38fe1bc433a511bbeeaba19
俞天诚 <davidyu18800@gmail.com>
2019-03-13 12:53:24 -0700

    load scenes

---
+++ Assets/Scripts/GameMeunManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMeunManager : MonoBehaviour
+{
+    public GameObject startEndLesslGameB, startSurvivalGameB, exitGameB, loading;
+    private string EndlessGameS, SurvivalGameS;
+    public int time;
+    void Start()
+    {
+        loading.SetActive(false);
+    }
+    public void StartSurvivalGame()
+    {
+        SurvivalGameS = "Survival";
+        StartCoroutine(StartGameTimer(SurvivalGameS));
+    }
+
+    public void StartEndLesslGame()
+    {
+        EndlessGameS = "Endless";
+        StartCoroutine(StartGameTimer(EndlessGameS));
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGameload()
+    {
+        startSurvivalGameB.SetActive(false);
+        startEndLesslGameB.SetActive(false);
+        exitGameB.SetActive(false);
+        loading.SetActive(true);
+    }
+
+    public IEnumerator StartGameTimer(string mode)
+    {
+        StartGameload();
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(mode);
+    }
+}
